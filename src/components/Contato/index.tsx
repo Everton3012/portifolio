ae278acefa05f1f3770a641b13f011822e36b306
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-22 00:45:33 -0300

    contato

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,12 +1,23 @@
+import styles from "./styles.module.scss"
+import Link from "next/link";
 import { Form } from "./components/Form";
+import {BiLogoWhatsapp, BiMailSend, BiLogoGithub, BiLogoLinkedin} from "react-icons/bi"
 
 export const Contato = () => {
     return (
-        <footer>
-            <div></div>
-            <div>
+        <main className={styles.main}>
+            <h3>Contato</h3>
+            <footer id="contato" className={styles.footer}>
+            <div className={styles.links}>
+                <Link className={styles.whats} target="blank" href="https://api.whatsapp.com/send?phone=5571991264179"><BiLogoWhatsapp size={30} /></Link>
+                <Link  className={styles.linkedin}  target="blank" href={"https://www.linkedin.com/in/everton-brandao-16938922b/"} ><BiLogoLinkedin size={30}/></Link>
+                <Link  className={styles.github}  target="blank" href={"https://github.com/Everton3012"} > <BiLogoGithub size={30}/></Link>
+                <Link  className={styles.email}  target="blank" href={"mailto:evertonmoura137@outlook.com"}><BiMailSend size={30}/></Link>
+            </div>
+            <div className={styles.form}>
                 <Form />
             </div>
         </footer>
+        </main>
     );
 };
