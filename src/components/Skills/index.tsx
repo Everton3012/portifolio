8f75963b24765d8143057a85994e3f51ee861417
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-21 22:58:08 -0300

    skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -4,11 +4,32 @@ export const Skills = () => {
     return (
         <main id="skills">
             <section className={styles.flex}>
-                <div>
+                <div className={styles.hard}>
                     <h4>Hard Skills</h4>
+                    <div>
+                        <span>ReactJs</span>
+                        <span>NextJs</span>
+                        <span>Sass</span>
+                        <span>NodeJs</span>
+                        <span>ExpressJs</span>
+                        <span>NestJs</span>
+                        <span>MySQL</span>
+                        <span>PostgresSql</span>
+                        <span>TypeScript</span>
+
+                        <span>MongoDB</span>
+                    </div>
                 </div>
-                <div>
+                <div className={styles.soft}>
                     <h4>Soft Skills</h4>
+                    <div>
+                        <span>Resolução de problemas</span>
+                        <span>Autonomia</span>
+                        <span>Capacidade de organização</span>
+                        <span>Produtividade</span>
+                        <span>Criatividade</span>
+                        <span>Responsabilidade</span>
+                    </div>
                 </div>
             </section>
         </main>
